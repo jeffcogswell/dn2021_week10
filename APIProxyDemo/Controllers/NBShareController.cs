657abdb8c2469abb2c725dcaf0794e5f571af851
Jeff Cogswell <jeffcogs@gmail.com>
2022-02-09 09:41:53 -0500

    Added comment about strings vs JSON

--- APIProxyDemo/Controllers/NBShareController.cs
+++ APIProxyDemo/Controllers/NBShareController.cs
@@ -15,6 +15,14 @@ namespace APIProxyDemo.Controllers
 		[HttpGet]
 		public async Task<string> get()
 		{
+
+			// IMPORTANT:
+			// Notice I'm not bothering with making classes for the response. Ultimately all
+			// http requests are really just strings anyway. So I'm just reading the JSON as
+			// a string and passing it exactly as-is back out. The Angular app in turn will
+			// correctly read it as JSON. (Interestingly, the ASP.NET core seems to notice
+			// that the data is JSON and attaches the correct content type of application/json.)
+
 			HttpClient client = new HttpClient();
 			client.BaseAddress = new Uri("https://dashboard.nbshare.io/api/v1/");
 			HttpResponseMessage response = await client.GetAsync("apps/reddit");
